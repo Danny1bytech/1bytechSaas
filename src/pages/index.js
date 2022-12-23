aa673c42703d4f67d3560d5d3f9f6cb551775d44
DannyPreye <99923947+DannyPreye@users.noreply.github.com>
2022-12-23 14:21:36 +0100

    fix: resolved the database issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,8 +14,8 @@ const Home = () => {
   return (
     <LandingLayout>
       <Meta
-        title="NextJS SaaS Boilerplate"
-        description="A boilerplate for your NextJS SaaS projects."
+        title='1bytetech'
+        description='Welcome to 1bytetech – a premier software development company specializing in building custom websites for businesses of all sizes. Our team of experienced developers is dedicated to creating user-friendly, responsive, and SEO-friendly websites that help our clients attract and retain customers. Whether you need a simple brochure website or a more complex e-commerce platform, we have the skills and expertise to deliver results that meet your unique needs. Contact us today to learn more about our website development services.'
       />
       <Hero />
       <Features />
