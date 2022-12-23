678dc6b6996f9a11fec57ded0356702fcc11b5fe
DannyPreye <99923947+DannyPreye@users.noreply.github.com>
2022-12-23 15:48:15 +0100

    fix:resolved the login page

--- src/components/Meta/index.js
+++ src/components/Meta/index.js
@@ -10,28 +10,28 @@ const Meta = ({ author, description, keywords, noIndex, title }) => {
 
   return (
     <Head>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="title" content={title} />
-      <meta name="description" content={description} />
-      <meta name="keywords" content={keywords} />
-      <meta name="author" content={author} />
+      <meta charSet='utf-8' />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta name='title' content={'1bytech ' + title} />
+      <meta name='description' content={description} />
+      <meta name='keywords' content={keywords} />
+      <meta name='author' content={author} />
 
       <title>{title}</title>
 
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={url} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={`${url}/images/seo-cover.png`} />
+      <meta property='og:type' content='website' />
+      <meta property='og:url' content={url} />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={description} />
+      <meta property='og:image' content={`${url}/images/seo-cover.png`} />
 
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content={url} />
-      <meta property="twitter:title" content={title} />
-      <meta property="twitter:description" content={description} />
-      <meta property="twitter:image" content={`${url}/images/seo-cover.png`} />
+      <meta property='twitter:card' content='summary_large_image' />
+      <meta property='twitter:url' content={url} />
+      <meta property='twitter:title' content={title} />
+      <meta property='twitter:description' content={description} />
+      <meta property='twitter:image' content={`${url}/images/seo-cover.png`} />
 
-      {noIndex && <meta name="robots" content="noindex" />}
+      {noIndex && <meta name='robots' content='noindex' />}
     </Head>
   );
 };
--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -51,32 +51,32 @@ const Login = () => {
   return (
     <AuthLayout>
       <Meta
-        title="NextJS SaaS Boilerplate | Login"
-        description="A boilerplate for your NextJS SaaS projects."
+        title='1bytech| Login'
+        description='A boilerplate for your NextJS SaaS projects.'
       />
-      <div className="flex flex-col items-center justify-center p-5 m-auto space-y-5 rounded shadow-lg md:p-10 md:w-1/3">
+      <div className='flex flex-col items-center justify-center p-5 m-auto space-y-5 rounded shadow-lg md:p-10 md:w-1/3'>
         <div>
-          <Link href="/">
-            <a className="text-4xl font-bold">Nextacular</a>
+          <Link href='/'>
+            <a className='text-4xl font-bold'>1bytech</a>
           </Link>
         </div>
-        <div className="text-center">
-          <h1 className="text-2xl font-bold">Sign in with your email</h1>
-          <h2 className="text-gray-600">
+        <div className='text-center'>
+          <h1 className='text-2xl font-bold'>Sign in with your email</h1>
+          <h2 className='text-gray-600'>
             We&apos;ll send a magic link to your inbox to confirm your email
             address and sign you in.
           </h2>
         </div>
-        <form className="flex flex-col w-full space-y-3">
+        <form className='flex flex-col w-full space-y-3'>
           <input
-            className="px-3 py-2 border rounded"
+            className='px-3 py-2 border rounded'
             onChange={handleEmailChange}
-            placeholder="user@email.com"
-            type="email"
+            placeholder='user@email.com'
+            type='email'
             value={email}
           />
           <button
-            className="py-2 text-white bg-blue-600 rounded hover:bg-blue-500 disabled:opacity-75"
+            className='py-2 text-white bg-blue-600 rounded hover:bg-blue-500 disabled:opacity-75'
             disabled={status === 'loading' || !validate || isSubmitting}
             onClick={signInWithEmail}
           >
@@ -89,12 +89,12 @@ const Login = () => {
         </form>
         {socialProviders.length > 0 && (
           <>
-            <span className="text-sm text-gray-400">or sign in with</span>
-            <div className="flex flex-col w-full space-y-3">
+            <span className='text-sm text-gray-400'>or sign in with</span>
+            <div className='flex flex-col w-full space-y-3'>
               {socialProviders.map((provider, index) => (
                 <button
                   key={index}
-                  className="py-2 bg-gray-100 border rounded hover:bg-gray-50 disabled:opacity-75"
+                  className='py-2 bg-gray-100 border rounded hover:bg-gray-50 disabled:opacity-75'
                   disabled={status === 'loading'}
                   onClick={() => signInWithSocial(provider.id)}
                 >
